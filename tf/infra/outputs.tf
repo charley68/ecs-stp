4d20a0fe7c9b6d4265fdee744991f86ea48bc73a
Stephen Lane <steve@stevemac.local>
2025-03-26 18:26:55 +0000

    Remove SG from infra

--- tf/infra/outputs.tf
+++ tf/infra/outputs.tf
@@ -14,10 +14,6 @@ output "public_subnet_id" {
   value = aws_subnet.public_subnet.id
 }
 
-output "security_group_id" {
-  value = aws_security_group.ecs_sg.id
-}
-
 output "ecs_cluster_name" {
   value = aws_ecs_cluster.main.name
 }
